c9132e084e39248c01fbe7fb032d76dc63d4ef59
danielkwon89 <danielkwon89@gmail.com>
2021-01-21 15:13:56 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ export default class App extends Component {
     constructor() {
         super()
         this.state = {
-            fetchedData: ""
+            peopleInSpace: []
         }
     }
 
     render() {
-        return("")
+        return(
+            <div>
+                {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+            </div>
+        )
     }
 
     componentDidMount() {
         fetch("http://api.open-notify.org/astros.json")
         .then(res => res.json())
-        .then(data => this.setState({fetchedData: data}))
+        .then(({people}) => this.setState({peopleInSpace: people}))
     }
 
 }
